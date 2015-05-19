517657ef8cf8dd95fe6115938274e40b04f89382
Alan Butler <rabutler@usbr.gov>
2015-05-19 17:24:33 -0600

    Edited to return diff. value for each scenario

--- code/dataTaggingFunctions.R
+++ code/dataTaggingFunctions.R
@@ -5,20 +5,16 @@
 # I.C. that come from MTOM using the min/most/max probable forecast use 2
 # I.C. that come from 24-MS using the min/most/max probable forecast use 3
 # I.C. are assumed to be the 5 dimension in the scenario names
+# assumes it is getting a vector of multiple scenarios.
 
 aggBasedOnIC <- function(scen)
 {
-  scen <- simplify2array(strsplit(as.character(scen),',',fixed = T))[5]
-  
-  if(scen %in% 1981:2010){
-    zz <- 1
-  } else if(scen %in% c('MTOM_Max','MTOM_Most','MTOM_Min')){
-    zz <- 2
-  } else if(scen %in% c('Min','Most','Max')){
-    zz <- 3
-  } else{
-    zz <- -99
-  }
+  scen <- simplify2array(strsplit(as.character(scen),',',fixed = T))[5,]
+
+  zz <- rep(-99,length(scen))
+  zz[scen %in% 1981:2010] <- 1
+  zz[scen %in% c('MTOM_Max','MTOM_Most','MTOM_Min')] <- 2
+  zz[scen %in% c('Min','Most','Max')] <- 3
   
   zz
 }
