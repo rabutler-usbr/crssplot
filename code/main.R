e287f8d33890bb64e5ac5261094c535e94850a27
Alan Butler <rabutler@usbr.gov>
2015-05-19 17:08:31 -0600

    Changed filename of getSysCondData

--- code/main.R
+++ code/main.R
@@ -1,14 +1,11 @@
 source('code/makeScenNames.R')
-source('code/getAprJanSysCondData.R')
+source('code/getSysCondData.R')
 source('code/dataTaggingFunctions.R')
 
 # script should create everything necessary for the results in order
 
 # scenarios are orderd model,supply,demand,policy,initial conditions
 scens <- makeAllScenNames('May2015_2016','DNF','2007Dems','IG',c(1981:2010,'MTOM_Most','Most'))
-
-#*** to remove after testing:
-scens <- scens[1:2]
 
 getSysCondData(scens, 'M:/Shared/CRSS/CRSS.2015/Scenario', 'C:/alan/CRSS/CRSS.2015/results/MaySysCond.txt', 
                addAggAttribute = TRUE, aggBasedOnIC)
