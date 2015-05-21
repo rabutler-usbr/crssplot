6a41ff0d94e686bdd4cef0376a7a853cb015fd3e
Alan Butler <rabutler@usbr.gov>
2015-05-21 13:42:21 -0600

    critical stats data
    * added function to get critical stats data
    * added table for which critical stats to get

--- code/getSysCondData.R
+++ code/getSysCondData.R
@@ -31,3 +31,32 @@ getSysCondData <- function(scens, iFolder, oFile, addAggAttribute = TRUE, aggFun
     write.table(zz, oFile)
   }
 }
+
+
+getSritStatsData <- function(scens, iFolder, oFile, addAggAttribute = TRUE, aggFunction)
+{
+  slotAggList <- RWDataPlot::createSlotAggList('data/CritStatsList.csv')
+  # first scens is the folder names to search, second is the names to save in the data file
+  # use folder names for now
+  print('starting to process scenarios...')
+  print(paste('this could take some time as you are processing',length(scens),'scenarios'))
+  flush.console()
+  
+  RWDataPlot::getDataForAllScens(scens,scens,slotAggList, iFolder, oFile)
+  
+  if(addAggAttribute){
+    print('now reading the file in...')
+    flush.console()
+    zz <- read.table(oFile,header=T)
+    
+    print('adding attribute...')
+    flush.console()
+    
+    zz <- dplyr::mutate(zz, Agg = aggFunction(Scenario))
+    
+    print('rewriting file...')
+    flush.console()
+    
+    write.table(zz, oFile)
+  }
+}
