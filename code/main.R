c5c159bdc745946db0c8be9d9b451ebcf531085f
Alan Butler <rabutler@usbr.gov>
2015-05-21 13:42:34 -0600

    get critical stats added

--- code/main.R
+++ code/main.R
@@ -14,17 +14,25 @@ curMonthPEFile <- 'May_MeadPowellPE.txt'
 pICFile <- '../CRSS.2015/MTOM/MTOM_May15_PowellPE.csv'
 mICFile <- '../CRSS.2015/MTOM/MTOM_May15_MeadPE.csv'
 icMonth <- '15-Dec' # IC are from December 2015
+critStatsFile <- 'May_CritStats.txt'
 
 ## System Conditions Table Data
 if(FALSE){
   getSysCondData(scens, iFolder, paste0(resFolder,sysConFile),TRUE, aggBasedOnIC)
 }
 
-## get the Mead and Powel EOCY Data
-getPowellMeadEOCYPE(scens, iFolder, paste0(resFolder,curMonthPEFile), TRUE, aggBasedOnIC)
-## append initial conditions onto May data
-getAndAppendIC(scens, paste0(resFolder,curMonthPEFile), pIcFile, mICFile, icMonth, 
-               TRUE, aggBasedOnIC)
+if(FALSE){
+  ## get the Mead and Powel EOCY Data
+  getPowellMeadEOCYPE(scens, iFolder, paste0(resFolder,curMonthPEFile), TRUE, aggBasedOnIC)
+  ## append initial conditions onto May data
+  getAndAppendIC(scens, paste0(resFolder,curMonthPEFile), pIcFile, mICFile, icMonth, 
+                 TRUE, aggBasedOnIC)
+}
+
+## Get Crit Stats Data
+if(TRUE){
+  getSritStatsData(scens, iFolder, paste0(resFolder,critStatsFile),TRUE, aggBasedOnIC)
+}
 
 # before combining with previous month, need to denote starting month of simulation?
 if(FALSE){
