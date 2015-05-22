5b69c4d7234cc716c1dcd42778e566847bd86862
Alan Butler <rabutler@usbr.gov>
2015-05-22 15:51:52 -0600

    added filterOn parameter
    allows to show either only traces that result in a shortage, or all
    traces below 1077.

--- code/plotFirstYearShortCond.R
+++ code/plotFirstYearShortCond.R
@@ -13,15 +13,23 @@ mtomPath <- 'MTOM/FirstYearCondMTOM/'
 iFile = paste0(mtomPath, dataYear,"Data",shortageYear,"Shortage.csv")
 oFile = paste0(mtomPath, dataYear,"Data",shortageYear,"Shortage.pdf")
 
-plotFirstYearShortCond <- function(iFile)
+# filterOn: valid choices are 'shortage', 'pe'
+# if filterOn == pe, then will keep all traces that are <= 1077
+plotFirstYearShortCond <- function(iFile, filterOn = 'shortage')
 {
   zz <- read.csv(iFile)
   
-  # trim to only traces with shortages
-  zz <- dplyr::filter(zz, Shortage == 1)
-  
+  if(filterOn == 'shortage'){
+    # trim to only traces with shortages  
+    zz <- dplyr::filter(zz, Shortage == 1)
+  } else if(filterOn == 'pe'){
+    zz <- dplyr::filter(zz, DecElev <= 1077)
+  } else {
+    stop('invalid filterOn passed to plotFirstYearShortCond')
+  }
+    
   zz$LBPrct <- paste0(round(zz$LBPrct,0),'%')
-  #zz$WYRelease <- zz$WYRelease/1000000
+  zz$WYRelease <- zz$WYRelease/1000
   zz$OND.Release <- as.factor(zz$OND.Release/1000)
   zz$HydrologyYear <- as.factor(zz$HydrologyYear)
 
@@ -32,13 +40,14 @@ plotFirstYearShortCond <- function(iFile)
   gg <- ggplot(zz, aes(HydrologyYear, DecElev, shape = OND.Release, color = WYRelease,
                        label = LBPrct))
   gg <- gg + geom_point(size = 4) + 
+    geom_hline(aes(yintercept = 1075),linetype = 2) + 
     scale_color_gradient(paste0('Powell WY ',dataYear,'\nRelease [MAF]'), low = 'red', 
                          high = 'blue', breaks = gradLabs) + 
-    geom_text(hjust = .4, vjust = -.6) + 
+    geom_text(hjust = .4, vjust = -.6,size = 3.5) + 
     labs(shape = paste0('Powell Oct-Dec\n',dataYear,' Release [MAF]'), x = paste0(dataYear,' Hydrology from Year'),
          y = paste0('Mead End-of-December ',dataYear,' elevation [ft]')) +
     scale_y_continuous(minor_breaks = 900:1200, breaks = seq(900,1200,1), label = comma) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-    coord_cartesian(ylim = c(round(min(zz$DecElev),0)-1.2,1075+.2))
+    coord_cartesian(ylim = c(round(min(zz$DecElev),0)-1.2,max(c(1075,zz$DecElev))+.5))
   gg
 }
