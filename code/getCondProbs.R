2ea6d1b3a885ebb8fff03fb504cf60f63d247f41
Alan Butler <rabutler@usbr.gov>
2015-05-22 13:25:45 -0600

    made getConditional probs into function

--- code/getCondProbs.R
+++ code/getCondProbs.R
@@ -3,58 +3,38 @@
 library(dplyr)
 library(reshape2)
 
-res <- read.table('results/SysConditions.txt', header = T)
-res <- tbl_df(res)
-res <- filter(res, Agg == 1)
+# res - results data frame from systemConditions.txt
+# Prob( yr1 cond1 | yr2 cond2) 
+# ex:
+# Prob(2016 Shortage | WY 2016 Release = 7.48) =
+#   getConditionalProbs(res, 2016, 2016, 'lbShortage','mer748')
+# P(2017 Short (by tier) | WY 2016 Rel = 7.48) = 
+#   getConditionalProbs(res, 2017, 2016, c('lbShortage','lbShortageStep1','lbshort2','lbshort3),
+#                       'mer748')
+getConditionalProbs <- function(res, yr1, yr2, cond1, cond2)
+{
+  if(yr1 == yr2){
+    zz <- dplyr::filter(res, Year == yr1, Variable %in% c(cond1, cond2))
+  } else{
+    zz <- rbind(dplyr::filter(res, Year == yr1, Variable %in% cond1),
+                dplyr::filter(res, Year == yr2, Variable %in% cond2))
+  }
 
-# P(2016 Short | WY 2016 Rel = 7.48)
-zz <- filter(res, Year == 2016, Variable %in% c('lbShortage', 'mer748'))
-zz <- zz %>% select(Value, Variable, Trace, Scenario) %>%
-  mutate(lName = paste(Scenario,Trace,sep = '_')) %>%
-  select(Value,Variable,lName)
-zz <- dcast(zz, lName ~ Variable, value.var = 'Value')
-zz2 <- filter(zz, mer748 == 1) 
-print(paste('P(2016 Short | WY 2016 Rel = 7.48)',mean(zz2$lbShortage)))
+  zz <- zz %>% select(Value, Variable, Trace, Scenario) %>%
+    mutate(lName = paste(Scenario,Trace,sep = '_')) %>% # create unique run, trace combination
+    select(Value,Variable,lName)
+  zz <- reshape2::dcast(zz, lName ~ Variable, value.var = 'Value')
 
-# P(2017 Short (by tier) | WY 2016 Rel = 7.48)
-zz <- filter(res,Year == 2016, Variable == 'mer748')
-zz2 <- filter(res,Year == 2017, Variable %in% c('lbShortage','lbShortageStep1','lbShortageStep2','lbShortageStep3'))
-zz <- rbind(zz,zz2)
-zz <- zz %>% select(Value, Variable, Trace, Scenario) %>%
-  mutate(lName = paste(Scenario,Trace,sep = '_')) %>%
-  select(Value,Variable,lName)
-zz <- dcast(zz, lName ~ Variable, value.var = 'Value')
-zz2 <- filter(zz,mer748 == 1)
-print(paste('P(2017 Short 1 | WY 2016 Rel = 7.48)',mean(zz2$lbShortageStep1)))
-print(paste('P(2017 Short 2 | WY 2016 Rel = 7.48)',mean(zz2$lbShortageStep2)))
-print(paste('P(2017 Short 3 | WY 2016 Rel = 7.48)',mean(zz2$lbShortageStep3)))
-print(paste('P(2017 Short | WY 2016 Rel = 7.48)',mean(zz2$lbShortage)))
-
-# P(2017 Short (by tier) | WY 2016 Rel = 8.23)
-zz <- filter(res,Year == 2016, Variable == 'ueb823')
-zz2 <- filter(res,Year == 2017, Variable %in% c('lbShortage','lbShortageStep1','lbShortageStep2','lbShortageStep3'))
-zz <- rbind(zz,zz2)
-zz <- zz %>% select(Value, Variable, Trace, Scenario) %>%
-  mutate(lName = paste(Scenario,Trace,sep = '_')) %>%
-  select(Value,Variable,lName)
-zz <- dcast(zz, lName ~ Variable, value.var = 'Value')
-zz2 <- filter(zz,ueb823 == 1)
-print(paste('P(2017 Short 1 | WY 2016 Rel = 8.23)',mean(zz2$lbShortageStep1)))
-print(paste('P(2017 Short 2 | WY 2016 Rel = 8.23)',mean(zz2$lbShortageStep2)))
-print(paste('P(2017 Short 3 | WY 2016 Rel = 8.23)',mean(zz2$lbShortageStep3)))
-print(paste('P(2017 Short | WY 2016 Rel = 8.23)',mean(zz2$lbShortage)))
-
-# P(2017 Short (by tier) | WY 2016 Rel > 8.23)
-zz <- filter(res,Year == 2016, Variable == c('eq'))
-zz3 <- filter(res, Year == 2016, Variable == 'uebGt823')
-zz2 <- filter(res,Year == 2017, Variable %in% c('lbShortage','lbShortageStep1','lbShortageStep2','lbShortageStep3'))
-zz <- rbind(zz,zz2,zz3)
-zz <- zz %>% select(Value, Variable, Trace, Scenario) %>%
-  mutate(lName = paste(Scenario,Trace,sep = '_')) %>%
-  select(Value,Variable,lName)
-zz <- dcast(zz, lName ~ Variable, value.var = 'Value')
-zz2 <- filter(zz,uebGt823 == 1 | eq == 1)
-print(paste('P(2017 Short 1 | WY 2016 Rel > 8.23)',mean(zz2$lbShortageStep1)))
-print(paste('P(2017 Short 2 | WY 2016 Rel > 8.23)',mean(zz2$lbShortageStep2)))
-print(paste('P(2017 Short 3 | WY 2016 Rel > 8.23)',mean(zz2$lbShortageStep3)))
-print(paste('P(2017 Short | WY 2016 Rel > 8.23)',mean(zz2$lbShortage)))
+  # can this be replaced using dplyr::filter?
+  rowKeep <- rep(F,nrow(zz))
+  for(i in 1:length(cond2)){
+    rowKeep <- rowKeep | zz[[cond2[i]]] == 1
+  }
+  #zz <- zz[zz[[cond2]] == 1,]
+  zz <- zz[rowKeep,]
+  retVal <- c()
+  for(i in 1:length(cond1)){
+    retVal[cond1[i]] <- mean(zz[[cond1[i]]])
+  }
+  retVal
+}
