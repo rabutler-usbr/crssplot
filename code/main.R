2f2d59b67e81536ce5e32c786e96c8e7f12a2c68
Alan Butler <rabutler@usbr.gov>
2015-05-22 11:52:49 -0600

    Updated through crit stats data
    includes:
    * creation of system condition table
    * EOCY elevation figures
    * critical stats figures and tables

--- code/main.R
+++ code/main.R
@@ -1,5 +1,6 @@
 library(CRSSIO)
 library(dplyr)
+library(reshape2)
 source('code/makeScenNames.R')
 source('code/getSysCondData.R')
 source('code/dataTaggingFunctions.R')
@@ -23,6 +24,9 @@ prevMonthPEFile <- 'April/April_MPPE_EOCY.txt'
 startMonthMap <- c('May2015_2016' = 'May 2015 DNF', 'Apr2015_2016_a3' = 'Apr 2015 DNF')
 oFigs <- '../CRSS.2015/figs/'
 eocyFigs <- 'May2015_MPEOCY.pdf'
+annText <- 'Results from the May 2015 CRSS Run'
+critStatsProc <- 'May_CritStats.csv'
+critFigs <- 'May2015_CritFigs.pdf'
 
 ## System Conditions Table Data
 if(FALSE){
@@ -42,7 +46,10 @@ if(FALSE){
   getSritStatsData(scens, iFolder, paste0(resFolder,critStatsFile),TRUE, aggBasedOnIC)
 }
 
-
+print("starting to create figures and tables")
+flush.console()
+print("creating system conditions table")
+flush.console()
 ## Create tables, figures, and data behind figures
 # 1) system conditions table
 sysCond <- read.table(paste0(resFolder,sysCondFile),header = T)
@@ -51,10 +58,12 @@ sysCond <- dplyr::filter(sysCond, Year %in% 2016:2026 & Agg == 1)
 # create the system cond. table
 sysTable <- CRSSIO::createSysCondTable(sysCond, 2016:2026)
 # save the sys cond table
-write.csv(sysTable[['fullTable']], paste0(resFolder,sysCondTable))
+write.csv(sysTable[['fullTable']], paste0(oFigs,sysCondTable))
 
 # 2) Plot Mead, Powell EOCY elvations and include previous month's results too.
 # read in current month data
+print("EOCY elevation figures")
+flush.console()
 peCur <- read.table(paste0(resFolder,curMonthPEFile),header = T)
 pePrev <- read.table(paste0(resFolder,prevMonthPEFile),header = T) # read in prev. month data
 # add start month attributes to both months' data
@@ -78,6 +87,53 @@ dev.off()
 
 rm(pe, powellPE, meadPE)
 
+
+# 3) Critical elevation thresholds; figures and data table
+# have sysCond for some, and read in crit stats for others
+print("starting critical stats")
+flush.console()
+critStats <- read.table(paste0(resFolder,critStatsFile),header = T)
+# defaults are ok for legendTitle, legLoc, nC, and annSize
+# filter to only use 30 ensemble and to drop Mead LT 1025 from one plot and Mead LT 1020 from 
+# the other plot
+critStatsFig1 <- plotCritStats(dplyr::filter(critStats, Agg == 1, Variable != 'meadLt1020'), 
+                               2016:2026, annText)
+critStatsFig2 <- plotCritStats(dplyr::filter(critStats, Agg == 1, Variable != 'meadLt1025'), 
+                               2016:2026, annText)
+# create data table to save crit stats
+cs <- dplyr::filter(critStats, Year %in% 2016:2026, Agg == 1)
+
+# rename the variables to strings
+cs$vName <- 'LB Shortage'
+cs$vName[cs$Variable == 'meadLt1000'] <- 'Mead < 1,000\' in Any Month'
+cs$vName[cs$Variable == 'meadLt1020'] <- 'Mead < 1,020\' in Any Month'
+cs$vName[cs$Variable == 'meadLt1025'] <- 'Mead < 1,025\' in Any Month'
+cs$vName[cs$Variable == 'powellLt3490'] <- 'Powell < 3,490\' in Any Month'
+
+# compute the percent of traces by averaging values 
+cs <- cs %>% group_by(Year,Variable,vName) %>%
+  summarise(Value = mean(Value))
+# reshape to be easier to print out
+cs <- reshape2::dcast(cs, Year ~ vName, value.var = 'Value')
+
+# shortage surplus figure
+# defaults ok for legendTitle, nC, and legLoc
+ssPlot <- plotShortageSurplus(dplyr::filter(sysCond, Variable %in% c('lbShortage', 'lbSurplus'),
+                                            Agg == 1), 2016:2026, 'May 2015')
+  
+# stacked barplot of different shortage tiers
+# default for annSize is ok
+shortStack <- plotShortStackedBar(dplyr::filter(sysCond, Variable %in% c('lbShortageStep1',
+                                  'lbShortageStep2','lbShortageStep3')), 2016:2026, annText)
+
+# save figures and table
+pdf(paste0(oFigs,critFigs),width = 8, height = 6)
+print(critStatsFig1)
+print(critStatsFig2)
+print(ssPlot)
+print(shortStack)
+dev.off()
+write.csv(cs,paste0(oFigs,critStatsProc),row.names = F)
 
 rm(list = ls())
 
