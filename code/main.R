c30cf80122da78433470718c37260a00a0737086
jshirey <jshirey@usbr.gov>
2016-05-19 15:34:48 -0600

    April 2016 CRSS run with 30 MTOM initial conditions

--- code/main.R
+++ code/main.R
@@ -1,3 +1,7 @@
+rm(list=ls())
+
+setwd("C:\\Users\\JShirey\\Desktop\\RCodes\\Process-CRSS-Res-master")
+
 library(CRSSIO)
 library(dplyr)
 library(reshape2)
@@ -9,6 +13,7 @@ source('code/getEOCYData.R')
 source('code/plottingFunctions.R')
 source('code/getCondProbs.R')
 source('code/plotFirstYearShortCond.R')
+source('code/get5YrTable.R')
 
 # script should create everything necessary for the results in order
 CRSSDIR=Sys.getenv("CRSS_DIR")
@@ -16,39 +21,38 @@ CRSSDIR=Sys.getenv("CRSS_DIR")
 #                                    USER INPUT
 # -------------------------------------------------------------------------------------
 # scenarios are orderd model,supply,demand,policy,initial conditions
-scens <- makeAllScenNames('Jun2015_2016','DNF','2007Dems','IG',c(1981:2010,'MTOM_Most','Most'))
+scens <- makeAllScenNames('Apr2016_2017','DNF','2007Dems','IG',c(1981:2010,'MTOM_Most'))
 # scens.limit should only include the 30 ensemble i.c. and not most/min/max runs
-scens.limit <- makeAllScenNames('Jun2015_2016','DNF','2007Dems','IG',1981:2010)
-iFolder <- 'M:/Shared/CRSS/CRSS.2015/Scenario' # folder with scenario folders created by RiverSMART
+scens.limit <- makeAllScenNames('Apr2016_2017','DNF','2007Dems','IG',1981:2010)
+iFolder <- paste0(CRSSDIR, '/Scenario') # folder with scenario folders created by RiverSMART
 resFolder <- paste0(CRSSDIR,'/results/') # folder to save procssed text files to (intermediate processed data)
-sysCondFile <- 'JunSysCond.txt' # file name of system conditions data
-curMonthPEFile <- 'Jun_MeadPowellPE.txt' # file name of Powell and Mead PE data
-pICFile <- paste0(CRSSDIR,'/MTOM/MTOM_Jun15_PowellPE.csv') # input file name of MTOM results for Powell PE
-mICFile <- paste0(CRSSDIR,'/MTOM/MTOM_Jun15_MeadPE.csv') # input file name of MTOM results for Mead PE
-icMonth <- '15-Dec' # IC are from December 2015
-critStatsFile <- 'Jun_CritStats.txt' # file name for critical stats data
-sysCondTable <- 'Jun_SysTableFull2016_2026.csv' # file name for the system conditions procssed file
-prevMonthPEFile <- 'April/April_MPPE_EOCY.txt' # file name that contains the previous CRSS run PE data
+sysCondFile <- 'AprSysCond_Test.txt' # file name of system conditions data
+curMonthPEFile <- 'Apr_MeadPowellPE.txt' # file name of Powell and Mead PE data
+pICFile <- paste0(CRSSDIR,'/MTOM/MTOM_APR16_PowellPE.csv') # input file name of MTOM results for Powell PE
+mICFile <- paste0(CRSSDIR,'/MTOM/MTOM_APR16_MeadPE.csv') # input file name of MTOM results for Mead PE
+icMonth <- '16-Dec' # IC are from December 2015
+critStatsFile <- '/Apr2016_CritStats.txt' # file name for critical stats data
+sysCondTable <- 'Apr_SysTableFull2016_2030.csv' # file name for the system conditions procssed file
+prevMonthPEFile <- 'Jan/Jan_MPPE_EOCY.txt' # file name that contains the previous CRSS run PE data
 # startMonthMap includes a map for the model name (from folder names), to a string that 
-# will show up on plots
-startMonthMap <- c('Jun2015_2016' = 'Jun 2015 DNF', 'May2015_2016' = 'May 2015 DNF', 
-	'Apr2015_2016_a3' = 'Apr 2015 DNF')
-oFigs <- paste0(CRSSDIR,'/figs/06_June/') # folder location to save figures and fully procssed tables
-eocyFigs <- 'Jun2015_MPEOCY.pdf' # file name for figure with Powell and Mead 10/50/90 EOCY elevations
-annText <- 'Results from the June 2015 CRSS Run' # text that will be added to figures
-critStatsProc <- 'Jun_CritStats.csv'
-critFigs <- 'Jun2015_CritFigs.pdf'
-condProbFile <- 'Jun_CondProbs.csv'
+# will show up on plots[]
+startMonthMap <- c('Apr2015_2016_a3' = 'Apr 2015 DNF','Jan2016' = 'Jan 2016 DNF')
+oFigs <- paste0(CRSSDIR,'/figs/April/') # folder location to save figures and fully procssed tables
+eocyFigs <- 'Apr2016_MPEOCY.pdf' # file name for figure with Powell and Mead 10/50/90 EOCY elevations
+annText <- 'Results from the April 2016 CRSS Run' # text that will be added to figures
+critStatsProc <- 'Apr2016_CritStats.csv'
+critFigs <- 'Apr2016_CritFigs2026.pdf'
+condProbFile <- 'Apr2016_CondProbs.csv'
 # mtom results file for creating conditions leading to shortage in 2016
-mtomResFile <- paste0(CRSSDIR,'/MTOM/FirstYearCondMTOM/JunMTOMResults.csv')
-shortCondFig <- 'shortConditionsFig.pdf'
+mtomResFile <- paste0(CRSSDIR,'/MTOM/FirstYearCondMTOM/AprilMTOMResults.csv') #changed to may b/c jun results file DNE
+shortCondFig <- 'Apr2016_shortConditionsFig.pdf'
 # for the 5-year simple table
-ss5 <- c('April CRSS', 'Jun CRSS')
+ss5 <- c('Jan CRSS', 'April CRSS')
 # should match files for critStatsFile:
-critStatsIn <- paste0(CRSSDIR,c('/results/April/CriticalElevationData_CRSS.Apr2015.csv',
-                 '/figs/Jun_CritStats.csv'))
-yy5 <- 2016:2020
-simple5YrFile <- 'Jun2015_5yrSimple.pdf'
+critStatsIn <- paste0(CRSSDIR,c('/results/Jan/Jan_CritStats_DNF.csv',
+                 '/figs/Apr2016_CritStats.csv'))
+yy5 <- 2017:2021
+simple5YrFile <- 'Apr2016_5yrSimple.pdf'
 createShortConditions <- FALSE
 
 # -------------------------------------------------------------------------------------
@@ -64,7 +68,7 @@ if(TRUE){
   flush.console()
 }
 
-if(FALSE){
+if(TRUE){
   ## get the Mead and Powel EOCY Data
   getPowellMeadEOCYPE(scens, iFolder, paste0(resFolder,curMonthPEFile), TRUE, aggBasedOnIC)
   ## append initial conditions onto May data
@@ -101,9 +105,9 @@ flush.console()
 # 1) system conditions table
 sysCond <- read.table(paste0(resFolder,sysCondFile),header = T)
 # trim to 2016-2026 and 30 trace ensemble I.C.; Agg == 1 limits to using 30 ensemble I.C. 
-sysCond <- dplyr::filter(sysCond, Year %in% 2016:2026 & Agg == 1)
+sysCond <- dplyr::filter(sysCond, Year %in% 2017:2030 & Agg == 1)
 # create the system cond. table
-sysTable <- CRSSIO::createSysCondTable(sysCond, 2016:2026)
+sysTable <- CRSSIO::createSysCondTable(sysCond, 2017:2030)
 # save the sys cond table
 write.csv(sysTable[['fullTable']], paste0(oFigs,sysCondTable))
 
@@ -121,9 +125,9 @@ pe <- rbind(peCur, pePrev)
 rm(peCur, pePrev)
 # plot
 # only use the 30 ensemble (Agg = 1)
-powellPE <- plotEOCYElev(dplyr::filter(pe, Agg == 1), 2015:2026, 'Powell.Pool Elevation', 
+powellPE <- plotEOCYElev(dplyr::filter(peCur, Agg == 1), 2017:2060, 'Powell.Pool Elevation', 
                          'Powell End-of-December Year Elevation')
-meadPE <- plotEOCYElev(dplyr::filter(pe, Agg == 1), 2015:2026, 'Mead.Pool Elevation', 
+meadPE <- plotEOCYElev(dplyr::filter(peCur, Agg == 1), 2017:2060, 'Mead.Pool Elevation', 
                          'Mead End-of-December Year Elevation')
 
 # save figures
@@ -166,12 +170,12 @@ cs <- reshape2::dcast(cs, Year ~ vName, value.var = 'Value')
 # shortage surplus figure
 # defaults ok for legendTitle, nC, and legLoc
 ssPlot <- plotShortageSurplus(dplyr::filter(sysCond, Variable %in% c('lbShortage', 'lbSurplus'),
-                                            Agg == 1), 2016:2026, 'May 2015')
+                                            Agg == 1), 2017:2026, 'April 2016')
   
 # stacked barplot of different shortage tiers
 # default for annSize is ok
 shortStack <- plotShortStackedBar(dplyr::filter(sysCond, Variable %in% c('lbShortageStep1',
-                                  'lbShortageStep2','lbShortageStep3')), 2016:2026, annText)
+                                  'lbShortageStep2','lbShortageStep3')), 2017:2026, annText)
 
 # save figures and table
 pdf(paste0(oFigs,critFigs),width = 8, height = 6)
@@ -187,32 +191,32 @@ write.csv(cs,paste0(oFigs,critStatsProc),row.names = F)
 # use sysCond
 if(is.na(match('sysCond',ls()))){
   sysCond <- read.table(paste0(resFolder,sysCondFile),header = T) 
-  sysCond <- dplyr::filter(sysCond, Year %in% 2016:2026 & Agg == 1)
-  sysTable <- CRSSIO::createSysCondTable(sysCond, 2016:2026)
+  sysCond <- dplyr::filter(sysCond, Year %in% 2017:2027 & Agg == 1)
+  sysTable <- CRSSIO::createSysCondTable(sysCond, 2017:2027)
 }
-cp1 <- getConditionalProbs(sysCond, 2016, 2016, 'lbShortage','mer748')
-cp2 <- getConditionalProbs(sysCond, 2016, 2016, 'lbShortage','ueb823')
-cp3 <- getConditionalProbs(sysCond, 2016,2016, 'lbShortage',c('eq','uebGt823'))
-cp4 <- getConditionalProbs(sysCond, 2017, 2016, c('lbShortage','lbShortageStep1','lbShortageStep2',
+cp1 <- getConditionalProbs(sysCond, 2017, 2017, 'lbShortage','mer748')
+cp2 <- getConditionalProbs(sysCond, 2017, 2017, 'lbShortage','ueb823')
+cp3 <- getConditionalProbs(sysCond, 2017,2017, 'lbShortage',c('eq','uebGt823'))
+cp4 <- getConditionalProbs(sysCond, 2018, 2017, c('lbShortage','lbShortageStep1','lbShortageStep2',
                                                'lbShortageStep3'), 'mer748')
-cp5 <- getConditionalProbs(sysCond, 2017, 2016, c('lbShortage','lbShortageStep1','lbShortageStep2',
+cp5 <- getConditionalProbs(sysCond, 2018, 2017, c('lbShortage','lbShortageStep1','lbShortageStep2',
                                                'lbShortageStep3'), 'ueb823')
-cp6 <- getConditionalProbs(sysCond, 2017, 2016, c('lbShortage','lbShortageStep1','lbShortageStep2',
+cp6 <- getConditionalProbs(sysCond, 2018, 2017, c('lbShortage','lbShortageStep1','lbShortageStep2',
                                                'lbShortageStep3'), c('eq','uebGt823'))
 
 # create data table from the above values
-cpt1 <- data.frame('ChanceOf' = c(paste(2016,names(cp1)),paste(2017,names(cp4))),
+cpt1 <- data.frame('ChanceOf' = c(paste(2017,names(cp1)),paste(2018,names(cp4))),
                    'PrctChance' = c(cp1,cp4))
 rr <- which(rownames(sysTable$fullTable) == 'Mid-Elevation Release Tier - annual release = 7.48 maf')
-cc <- which(colnames(sysTable$fullTable) == 2016)
+cc <- which(colnames(sysTable$fullTable) == 2017)
 cpt1$PowellWYRel <- paste('7.48 MAF;',sysTable$fullTable[rr,cc])
 
-cpt2 <- data.frame('ChanceOf' = c(paste(2016,names(cp2)),paste(2017,names(cp5))),
+cpt2 <- data.frame('ChanceOf' = c(paste(2017,names(cp2)),paste(2018,names(cp5))),
                    'PrctChance' = c(cp2,cp5))
 rr <- which(rownames(sysTable$fullTable) == "Upper Elevation Balancing - annual release = 8.23 maf")
 cpt2$PowellWYRel <- paste('8.23 MAF;',sysTable$fullTable[rr,cc])
 
-cpt3 <- data.frame('ChanceOf' = c(paste(2016,names(cp3)),paste(2017,names(cp6))),
+cpt3 <- data.frame('ChanceOf' = c(paste(2017,names(cp3)),paste(2018,names(cp6))),
                    'PrctChance' = c(cp3,cp6))
 rr <- which(rownames(sysTable$fullTable) == "Upper Elevation Balancing - annual release > 8.23 maf")
 rr2 <- which(rownames(sysTable$fullTable) == "Equalization - annual release > 8.23 maf")
