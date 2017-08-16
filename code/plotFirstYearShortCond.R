3b8c6e970c5a0eca2f9d4e41b53f8741a7d7ffb6
Alan Butler <rabutler@usbr.gov>
2017-08-16 15:39:15 -0600

    updated `plotFirstYearShortCond()` so that it can get the conditions from MTOM or CRSS
    - new arguements:
        - model, scenario
    - new functions:
        - `getCRSSConditionsData()`
        - `getMTOMConditionsData()`

--- code/plotFirstYearShortCond.R
+++ code/plotFirstYearShortCond.R
@@ -1,12 +1,10 @@
 
-library(dplyr)
-library(ggplot2)
+library(tidyverse)
 library(scales)
 library(grid)
+library(RWDataPlyr)
 
-# filterOn: valid choices are 'shortage', 'pe'
-# if filterOn == pe, then will keep all traces that are <= 1077
-plotFirstYearShortCond <- function(iFile, filterOn = 'shortage', dataYear)
+getMTOMConditionsData <- function(iFile, filterOn) 
 {
   zz <- read.csv(iFile)
   
@@ -18,11 +16,91 @@ plotFirstYearShortCond <- function(iFile, filterOn = 'shortage', dataYear)
   } else {
     stop('invalid filterOn passed to plotFirstYearShortCond')
   }
-    
+  
   zz$LBPrct <- paste0(round(zz$LBPrct,0),'%')
   zz$WYRelease <- zz$WYRelease/1000
   zz$OND.Release <- as.factor(zz$OND.Release/1000)
   zz$HydrologyYear <- as.factor(zz$HydrologyYear)
+  
+  zz
+}
+
+getCRSSConditionsData <- function(iFolder, scenario, filterOn, dataYear) 
+{
+  # mead PE in December dataYear
+  # Powell WY Rel in dataYear
+  # Powell OND Rel in dataYear
+  # LB side inflow in dataYear
+  sal <- createSlotAggList(matrix(
+    c("KeySlots.rdf", "Mead.Pool Elevation", "EOCY", NA, "DecElev",
+    "KeySlots.rdf", "PowellOperation.PowellWYRelease", "EOCY", .000001, "WYRelease",
+    "Check.rdf", "TotVal.Mead", "AnnualSum", NA, "nfAbvMead",
+    "Check.rdf", "TotVal.BelowMead", "AnnualSum", NA, "nfBelowMead"),
+    byrow = TRUE,
+    ncol = 5
+  ))
+  sal2 <- createSlotAggList(matrix(
+    c("KeySlots.rdf", "Powell.Outflow", "Monthly", NA, "powellOut"),
+    byrow = TRUE,
+    nrow = 1
+  ))
+  zz <- getDataForAllScens(scenario, scenario, sal, iFolder, "crssCond.feather", TRUE)
+  on.exit(file.remove("crssCond.feather"))
+  zzMon <- getDataForAllScens(scenario, scenario, sal2, iFolder, "crssCond2.feather", TRUE)
+  on.exit(file.remove("crssCond2.feather"), add = TRUE)
+  
+  zz <- zz %>%
+    filter(Year == dataYear) %>%
+    group_by(Scenario, Year, Trace) %>%
+    spread(Variable, Value) %>%
+    mutate(lbGains = nfAbvMead + nfBelowMead) %>%
+    select(-nfAbvMead, -nfBelowMead)
+  lbAvg <- mean(zz$lbGains)
+  
+  zzMon <- zzMon %>%
+    filter(Year == dataYear, Month %in% c("Oct", "Nov", "Dec")) %>%
+    group_by(Scenario, Trace, Year, Variable) %>%
+    summarise(Value = sum(Value)/1000000) %>% # now OND total release 
+    group_by(Scenario, Trace, Year) %>%
+    spread(Variable, Value)
+  
+  message("Assuming that ", scenario, " uses full observed hydrology\n",
+          "If this is not the case, plotFirstYearShortCond() needs to be revisited")
+  
+  zz <- full_join(zz, zzMon, by = c("Scenario", "Trace", "Year")) %>%
+    ungroup() %>%
+    select(-Scenario, -Year) %>%
+    mutate(lbGains = paste0(round(lbGains/lbAvg*100,0), "%"),
+           Trace = as.factor(Trace + 1905),
+           powellOut = as.factor(powellOut),
+           WYRelease = round(WYRelease,2)) %>%
+    rename(LBPrct = lbGains, OND.Release = powellOut, HydrologyYear = Trace)
+    
+  
+  if(filterOn == 'shortage'){
+    # trim to only traces with shortages  
+    zz <- dplyr::filter(zz, DecElev <= 1075)
+  } else if(filterOn == 'pe'){
+    zz <- dplyr::filter(zz, DecElev <= 1077)
+  } else {
+    stop('invalid filterOn passed to plotFirstYearShortCond')
+  }
+  on.exit()
+  zz
+}
+
+# filterOn: valid choices are 'shortage', 'pe'
+# if filterOn == pe, then will keep all traces that are <= 1077
+plotFirstYearShortCond <- function(model, iFile, scenario, filterOn = 'shortage', dataYear)
+{
+  if(model == "MTOM"){
+    zz <- getMTOMConditionsData(iFile, filterOn)
+  } else if(model == "CRSS"){
+    zz <- getCRSSConditionsData(iFile, scenario, filterOn, dataYear)
+  } else {
+    stop("invalid model passed to plotFirstYearShortCond")
+  }
+  
 
   gradLabs <- round(range(zz$WYRelease),1)
   gradLabs <- round(seq(gradLabs[1],gradLabs[2],length = 5),1)
