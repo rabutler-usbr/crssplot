66e3bbfd5ec35a3d2a78b95c1c99ce5ab79719db
Alan Butler <rabutler@usbr.gov>
2020-02-06 13:43:11 -0700

    update to allow for multiple input folders

--- code/rename_excel_files.R
+++ code/rename_excel_files.R
@@ -5,39 +5,50 @@ library(assertthat)
 #' `rename_excel_file()` inserts a scenario name to the beginning of an the 
 #' existing file name.
 #' 
-#' @param ifolder Top level folder to find the excel files.
+#' @param ifolder Top level folder(s) to find the excel files.
 #' 
-#' @param files Vector of file names to rename. 
+#' @param files Vector of file(s) to rename. 
 #' 
 #' @param insert_name By default (if this is `NULL`), the top level folder name
 #'   will be inserted into the current file names. If this parameter is
-#'   specified, it will be used instead of the top level folder.
+#'   specified, it will be used instead of the top level folder. If it is not 
+#'   `NULL`, it must be the same length as the number of files specified by 
+#'   `ifolder`.
 
 rename_excel_file <- function(files, ifolder, insert_name = NULL)
 {
-  assert_that(length(ifolder) == 1)
-  assert_that(dir.exists(ifolder))
-  old_files <- file.path(ifolder, files)
-  assert_that(all(file.exists(old_files)))
+  #assert_that(length(ifolder) == 1)
+  assert_that(all(dir.exists(ifolder)))
+  file_folder_combo <- expand.grid(ifolder, files)
+  all_files <- paste(file_folder_combo[,1], file_folder_combo[,2], sep = "/")
+  assert_that(all(file.exists(all_files)))
   
-  if (is.null(insert_name)) {
-    insert_name <- stringr::str_split(ifolder, "/")[[1]]
+  for (i in seq_along(ifolder)) {
     
-    # don't know if there was a trailing "/" in the folder, so if there was, 
-    # we need to not use the last value, otherwise we want the last value
+    ff <- ifolder[i]
+    old_files <- file.path(ff, files)
     
-    if (tail(insert_name, 1) == "") {
-      insert_name <- tail(insert_name, 2)[1]
+    if (is.null(insert_name)) {
+      tmp_name <- stringr::str_split(ff, "/")[[1]]
+      
+      # don't know if there was a trailing "/" in the folder, so if there was, 
+      # we need to not use the last value, otherwise we want the last value
+      
+      if (tail(tmp_name, 1) == "") {
+        tmp_name <- tail(tmp_name, 2)[1]
+      } else {
+        tmp_name <- tail(tmp_name, 1)
+      }
     } else {
-      insert_name <- tail(insert_name, 1)
+      tmp_name <- insert_name[i]
     }
+    
+    # now loop through all files, and rename them
+    
+    new_files <- file.path(ff, paste0(tmp_name, "-", files))
+    
+    file.rename(old_files, new_files)
   }
   
-  # now loop through all files, and rename them
-  
-  new_files <- file.path(ifolder, paste0(insert_name, "-", files))
-  
-  file.rename(old_files, new_files)
-  
-  invisible(files)
+  invisible(all_files)
 }
