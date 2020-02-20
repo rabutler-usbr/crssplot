6c6a1587665901fa06e0756ef89969ff037ea92d
Alan Butler <rabutler@usbr.gov>
2020-02-20 15:27:57 -0700

    yaml now parses the scenarios, except for shortage conditions and pe_scatter
    For the scenario specification, name, folder, and ic can be r statements
    
    #100

--- code/parse_yaml_input.R
+++ code/parse_yaml_input.R
@@ -1,17 +1,19 @@
 library(yaml)
 #library(fs) # for is_absolute_path()
 library(assertthat)
+library(purrr)
 
 parse_yaml_input <- function(file)
 {
   zz <- yaml.load_file(file)
   
   assert_that(all(required_entries("top") %in% names(zz)))
-  
+
   zz <- zz %>%
     set_defaults() %>%
     set_process_data() %>%
-    set_folders()
+    set_folders() %>%
+    set_scenarios()
   
   zz
 }
@@ -31,6 +33,14 @@ set_defaults <- function(ui)
     # set defaults to defaults
     ui[["defaults"]] <- get_global_defaults()
   }
+  
+  # TODO: also need to set the following
+  # from old specification code:
+  # # TODO: update so that these are computed if not specified
+  # # years to show the crit stats figures  
+  # defaults[['plot_yrs']] <- defaults$start_year:defaults$end_year 
+  # # years to show the Mead/Powell 10/50/90 figures for
+  # defaults[['pe_yrs']] <- (defaults$start_year - 1):defaults$end_year
   
   ui
 }
@@ -60,6 +70,8 @@ required_entries <- function(level) {
     rv <- c("i_folder", "CRSSDIR", "crss_month", "pdf_name")
   } else if (level == "process_data") {
     rv <- c("sys_cond_data", "pe_data", "csd_dat", "crss_short_cond_data")
+  } else if (level == "scenarios") {
+    rv <- c("name", "folder", "ic", "start_year")
   }
   
   rv
@@ -139,4 +151,194 @@ set_folders <- function(ui)
   }
   
   ui
+}
+
+# parse the secenarios entry and create scenarios$scens, scenarios$ic_list
+# scenarios$ic_month, ind_plots, and scen_tree
+set_scenarios <- function(ui)
+{
+  ss <- ui[["scenarios"]]
+  assert_that(length(ss) >= 1)
+  
+  scens <- list()
+  ic_list <- list()
+  ic_month <- list()
+  ind_plots <- list()
+  scen_tree <- list()
+  
+  # loop through all scenarios 
+  req_names <- required_entries("scenarios")
+  
+  all_scenarios <- c()
+  std_ind_fig_ops <- list()
+  
+  for (cur_scen in ss) {
+    # check that it has all required columns
+    assert_that(all(req_names %in% names(cur_scen)))
+    
+    cur_scen <- check_scenarios_specification(cur_scen) %>%
+      eval_scenarios_specification() %>%
+      check_std_ind_figures(ui[["defaults"]]) %>%
+      check_std_ind_tables()
+
+    all_scenarios <- c(
+      all_scenarios,
+      create_scenario(
+        cur_scen[["name"]],
+        scen_folders = cur_scen[["folder"]],
+        ic = cur_scen[["ic"]],
+        start_year = cur_scen[["start_year"]],
+        std_ind_tables = cur_scen[["std_ind_tables"]],
+        std_ind_figures = cur_scen[["std_ind_figures"]][["create"]]
+      )
+    )
+    
+    # if creating std figures, then update its options
+    if (cur_scen[["std_ind_figures"]][["create"]]) {
+      std_ind_fig_ops[[cur_scen[["name"]]]] <- list(
+        ann_text = cur_scen[["std_ind_figures"]][["ann_text"]],
+        end_year = cur_scen[["std_ind_figures"]][["end_year"]]
+      )
+    }
+  }
+  
+  ui[["scenarios"]] <- scenario_to_vars(all_scenarios)
+  ui[["ind_plots"]] <- specify_individual_plots(
+    all_scenarios, 
+    std_ind_fig_ops, 
+    ui[["defaults"]]
+  )
+  ui[["scen_tree"]] <- all_scenarios
+  
+  ui
+}
+
+# checks a single scenario specification
+check_scenarios_specification <- function(scen)
+{
+  assert_that(is.character(scen[["name"]]) && length(scen[["name"]]) == 1)
+  assert_that(is.character(scen[["folder"]]))
+  assert_that(is.numeric(scen[["start_year"]]) && 
+                length(scen[["start_year"]]) == 1)
+  
+  if (is.list(scen[["ic"]])) {
+    assert_that(all(names(scen[["ic"]]) %in% c("powell", "mead")))
+  } else {
+    assert_that(is.character(scen[["ic"]]) && length(scen[["ic"]]) == 1)
+  }
+  
+  invisible(scen)
+}
+
+# checks if any of the values are r statements, and if they are, evaluates
+# them. Also, if ic is explicitly specified for powell and mead, converts that
+# to a vector (from list)
+eval_scenarios_specification <- function(scen)
+{
+  # check name, folder, and ic
+  scen <- eval_r_var(scen, "name") %>%
+    eval_r_var("folder")
+  
+  if (is.list(scen[["ic"]])) {
+    # convert to vector
+    scen[["ic"]] <- c(scen[["ic"]][["powell"]], scen[["ic"]][["mead"]])
+  } else {
+    scen <- eval_r_var(scen, "ic")
+  }
+  
+  scen
+}
+
+# Check to see if entry is an r statement, if it is, then it evaluates the r
+# statment. Otherwise it does nothing. 
+# We assume that the entry has already been checked to see if it is a character
+# scalar, i.e., is_r_statement assumes that it is checking a string of length 
+# == 1.
+eval_r_var <- function(scen, entry)
+{
+  if (is_r_statement(scen[[entry]])) {
+    # strip of `r and `
+    tmp <- scen[[entry]] %>%
+      strip_r_from_string() %>%
+      parse(text = ., keep.source = FALSE) %>%
+      eval()
+    
+    scen[[entry]] <- tmp
+  }
+  
+  scen
+}
+
+strip_r_from_string <- function(x)
+{
+  x <- sub("^`r\\s", "", x)
+  x <- sub("`$", "", x)
+  
+  x
+}
+
+#' Checks that the single character vector is an r statement, as denoted by
+#' "`r statement`"
+is_r_statement <- function(x)
+{
+  grepl("`r\\s.{1,}`", x)
+}
+
+# checks to see if user specified that std_ind_figures should be created
+# if it does, checks other optional parameters, and sets to default values
+check_std_ind_figures <- function(scen, defaults)
+{
+  if (exists("std_ind_figures", scen)) {
+    
+    sif <- scen[["std_ind_figures"]]
+    
+    # it can be either a scalar logical or a list
+    assert_that(is_scalar_logical(sif) || is.list(sif))
+    
+    if (isTRUE(sif) || 
+        (is.list(sif) && is.null(sif[["create"]])) || 
+        isTRUE(sif[["create"]])) {
+      # if it is not a list, make it a list
+      if (!is.list(sif))
+        sif <- list()
+      # set create if it is not already set
+      sif[["create"]] <- TRUE
+      # ann_text can be specified; if it is, it should be scalar character 
+      # or null
+      if (exists("ann_text", where = sif))
+        assert_that(is_scalar_character(sif[["ann_text"]]) || 
+                      is.null(sif[["ann_text"]]))
+      
+      # end_year can be specified; if it is, it should be scalar int; if it is
+      # not, then set it to defaults
+      if (exists("end_year", where = sif)) {
+        assert_that(is_scalar_integer(sif[["end_year"]]))
+      } else {
+        sif[["end_year"]] <- defaults[["end_year"]]
+      }
+      
+      scen[["std_ind_figures"]] <- sif
+      
+    } else if (isFALSE(sif)) {
+      scen[["std_ind_figures"]] <- list()
+      scen[["std_ind_figures"]][["create"]] <- FALSE
+    }
+    
+  } else {
+    scen[["std_ind_figures"]] <- list()
+    scen[["std_ind_figures"]][["create"]] <- FALSE
+  }
+  
+  scen
+}
+
+check_std_ind_tables <- function(scen)
+{
+  if (exists("std_ind_tables", scen)) {
+    assert_that(is_scalar_logical(scen[["std_ind_tables"]]))
+  } else {
+    scen[["std_ind_tables"]] <- FALSE
+  }
+  
+  scen
 }
--- code/test_parse_yaml.R
+++ code/test_parse_yaml.R
@@ -1,6 +1,7 @@
 # initial tests
 library(testthat)
 library(yaml)
+library(RWDataPlyr)
 
 ft <- yaml.load_file("test.yml")
 us <- yaml.load_file("test_underspecified.yml")
@@ -38,4 +39,54 @@ test_that("process_data specification works", {
 test_that("folders specification works", {
   us_tmp <- set_folders(us)
   expect_equal(us_tmp[["folders"]][["extra_label"]], "")
+})
+
+# is_r_statement ----------------------
+test_that("is_r_statement() works", {
+  expect_true(is_r_statement("`r mean(c(1,2))`"))
+  expect_true(is_r_statement("`r mean(c(1,2)) + 5`"))
+  expect_true(is_r_statement("`r sumfun();nextfun()`"))
+  expect_true(is_r_statement("`r mean(median(max(5)))`"))
+  expect_true(is_r_statement("`r mean(c(1,2))%>%anotherfun()`"))
+  expect_false(is_r_statement("string"))
+  expect_false(is_r_statement("r this()"))
+  expect_false(is_r_statement("`r this()"))
+  expect_false(is_r_statement("r this()`"))
+  expect_false(is_r_statement("`this()`"))
+  expect_false(is_r_statement("r is new"))
+  expect_false(is_r_statement("`r` is my language of choice"))
+})
+
+# strip_r_from_string -------------------
+test_that("strip_r_from_string() works", {
+  expect_identical(strip_r_from_string("`r mean()`"), "mean()")
+  expect_identical(strip_r_from_string("`r mean(c(1,2))`"), "mean(c(1,2))")
+  expect_identical(strip_r_from_string("`r df$x`"), "df$x")
+  expect_identical(strip_r_from_string("`r x$`r is nice``"), "x$`r is nice`")
+})
+
+# eval_r_var --------------
+df1 <- list(a = month.abb, b = "`r mean(1:12)`", c = "mean(1:12)")
+df2 <- list(
+  a = month.abb, 
+  b = "`r rw_scen_gen_names('DNF', 'CT', c('IG', 'NA'))`",
+  c = "`r month.name`"
+)
+df1_comp <- list(a = month.abb, b = mean(1:12), c = "mean(1:12)")
+df2_comp <- list(
+  a = month.abb,
+  b = rw_scen_gen_names("DNF", "CT", c("IG", "NA")),
+  c = month.name
+)
+
+test_that("eval_r_var() works", {
+  expect_equal(
+    eval_r_var(df1, "b") %>% eval_r_var(entry = "c"),
+    df1_comp
+  )
+  
+  expect_equal(
+    eval_r_var(df2, entry = "b") %>% eval_r_var(entry = "c"),
+    df2_comp
+  )
 })
