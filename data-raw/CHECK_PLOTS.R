49f55c1fb59627d55ce5d7d8ea3ab5ae71482712
Alan Butler <rabutler@usbr.gov>
2020-12-23 15:01:48 -0700

    added tests and check plots for scens_plot_cloud
    #4

--- data-raw/CHECK_PLOTS.R
+++ data-raw/CHECK_PLOTS.R
@@ -59,5 +59,30 @@ print(scens_plot_probs(
   facet_ncol = 2
 ))
 
-dev.off()
+# scens_plot_cloud() ------------------------------------------
 
+h_mead <- read.csv("inst/extdata/HistMeadPE.csv")
+h_powell <- read.csv("inst/extdata/HistPowellPE.csv")
+hh <- h_mead
+colnames(hh)[2] <- "mead_dec_pe"
+hh$powell_dec_pe <- h_powell[,2]
+
+pal <- c(
+  "April ST 2007 UCRC" = "#138d75",
+  "April ST CT" = "#f1c40f"
+)
+
+p1 <- scens_plot_cloud(ex_pe, "mead_dec_pe", historical = h_mead, legend_wrap = 20,
+                 plot_colors = pal, y_lab = "feet", years = 2020:2026) +
+  theme_cloud()
+
+p2 <- scens_plot_cloud(ex_pe, c("powell_dec_pe", "mead_dec_pe"), 
+                       historical = hh, legend_wrap = 20,
+                       plot_colors = pal, y_lab = "feet", years = 2020:2026,
+                       facet_scales = "free_y", fill_label = "ok then") +
+  theme_cloud()
+
+print(p1)
+print(p2)
+
+dev.off()
--- tests/testthat/test-scens_plot_cloud.R
+++ tests/testthat/test-scens_plot_cloud.R
@@ -1,12 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
 
 # scens_plot_cloud <- function(df, vars, historical = NULL, years = NULL, 
 #                              scenarios = NULL, plot_colors = NULL, 
 #                              scen_labels = NULL, ...)
 
-h_mead <- read.csv("inst/extdata/HistMeadPE.csv")
+h_mead <- read.csv(system.file("extdata/HistMeadPE.csv", package = "crssplot"))
+h_powell <- read.csv(
+  system.file("inst/extdata/HistPowellPE.csv", package = "crssplot")
+)
+hh <- h_mead
+colnames(hh)[2] <- "mead_dec_pe"
+hh$powell_dec_pe <- h_powell[,2]
 
 pal <- c(
   "April ST 2007 UCRC" = "#138d75",
@@ -17,32 +20,28 @@ scens_plot_cloud(ex_pe, "mead_dec_pe", historical = h_mead, legend_wrap = 20,
                  plot_colors = pal, y_lab = "feet", years = 2020:2026) +
   theme_cloud()
 
-
-tst <- data.frame(x = 1:10, mid = 1:10, low = 1:10 - 5, top = 1:10 + 6, cat = "yeah", stringsAsFactors = FALSE)
-tst <- bind_rows(tst, data.frame(x = 1:10, mid = 1:10 - 2, cat = "nah", stringsAsFactors = FALSE))
-
-gg <- ggplot(filter(tst, cat == "yeah"), aes(x)) +
-  geom_ribbon(aes(ymin = low, ymax = top, fill = cat, color = cat), linetype = 2, alpha = 0.2) +
-  geom_line(data = tst, aes(y = mid, color = cat))
-
-ggfill <- ggplot(filter(tst, cat == "yeah"), aes(x)) +
-  geom_ribbon(aes(ymin = low, ymax = top, fill = cat, color = cat), linetype = 2, alpha = 0.2)
-
-ggline <- ggplot(tst, aes(x)) +
-  geom_line(aes(y = mid, color = cat))
-
-
-legfill <- ggplotGrob(ggfill)$grobs[[which(sapply(ggplotGrob(ggfill)$grobs, function(x) x$name) == "guide-box")]]
-legline <- ggplotGrob(ggline)$grobs[[which(sapply(ggplotGrob(ggline)$grobs, function(x) x$name) == "guide-box")]]
-
-l2 <- rbind(legfill, legline)
-
-leg <- ggplotGrob(gg)$grobs[[which(sapply(ggplotGrob(gg)$grobs, function(x) x$name) == "guide-box")]]
-
-i <- which(sapply(ggplotGrob(gg)$grobs, function(x) x$name) == "guide-box")
-
-ggg <- ggplotGrob(gg)
-gtable::gtable_add_grob(ggg$grobs[[i]], legfill, t = 3, l = 3)
-
-
-cowplot::plot_grid(gg, legfill, rel_widths = c(2,.4))
+test_that("scens_plot_cloud() works", {
+  expect_s3_class(
+    ggplot_build(scens_plot_cloud(ex_pe, "mead_dec_pe")), 
+    "ggplot_built"
+  )
+  
+  expect_s3_class(
+    ggplot_build(scens_plot_cloud(ex_pe, "mead_dec_pe", historical = h_mead, 
+                                  legend_wrap = 20,
+                                  plot_colors = pal, y_lab = "feet", 
+                                  years = 2020:2026)), 
+    "ggplot_built"
+  )
+  
+  expect_s3_class(
+    ggplot_build(
+      scens_plot_cloud(ex_pe, c("powell_dec_pe", "mead_dec_pe"), 
+                      historical = hh, legend_wrap = 20,
+                      plot_colors = pal, y_lab = "feet", 
+                      years = 2020:2026,
+                      facet_scales = "free_y", fill_label = "ok then")
+    ), 
+    "ggplot_built"
+  )
+})
