bbbb1cf8a7da8efbba11763b2650c074f827c102
Alan Butler <rabutler@usbr.gov>
2021-03-05 11:15:37 -0700

    removed "5-year simple table"

--- R/get_plot_flags.R
+++ R/get_plot_flags.R
@@ -4,7 +4,6 @@ get_plot_flags <- function(ui)
   std_comparison <- FALSE
   heat <- FALSE
   cloud <- FALSE
-  simple_5yr <- FALSE
   mead_pe_scatter <- FALSE
   shortage_conditions <- FALSE
   
@@ -13,7 +12,6 @@ get_plot_flags <- function(ui)
     std_comparison <- std_comparison | ui[["plot_group"]][[i]][["std_comparison"]][["create"]]
     heat <- heat | ui[["plot_group"]][[i]][["heat"]][["create"]]
     cloud <- cloud | ui[["plot_group"]][[i]][["cloud"]][["create"]]
-    simple_5yr <- simple_5yr | ui[["plot_group"]][[i]][["simple_5yr"]][["create"]]
   }
   
   cond_probs <- FALSE
