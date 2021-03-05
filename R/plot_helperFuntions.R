cdb195f884f2ff94dfefde2b38219a5e1579a278
Alan Butler <rabutler@usbr.gov>
2021-03-05 11:17:38 -0700

    moved determine_plot_colors

--- R/plot_helperFuntions.R
+++ R/plot_helperFuntions.R
@@ -19,3 +19,22 @@ myBoxStats1090 <- function(x)
   names(r) <- c("ymin", "lower", "middle", "upper", "ymax")
   r
 }
+
+
+determine_plot_colors <- function(plot_colors, col_vars, 
+                                  msg_string = "Scenario")
+{
+  if (is.null(plot_colors)) {
+    plot_colors <- scales::hue_pal()(length(col_vars))
+    names(plot_colors) <- col_vars
+  } else {
+    # check that there are specified colors for each Variable
+    assert_that(
+      all(col_vars %in% names(plot_colors)), 
+      msg = paste(msg_string, "names not found in `plot_colors`")
+    )
+  }
+  
+  plot_colors
+}
+
